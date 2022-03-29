98254183f1ead05d0db7fabf6876ccfa7daa0125
amanjaiswal03 <aman.jaiswal@code.berlin>
2022-03-29 11:47:12 +0200

    Made hashed_password to accept null values for google Auth to work and fixed OAuth bugs

--- backend/lib/sportpal_web/controllers/google_auth_controller.ex
+++ backend/lib/sportpal_web/controllers/google_auth_controller.ex
@@ -1,13 +1,35 @@
 defmodule SportpalWeb.GoogleAuthController do
     use SportpalWeb, :controller
+    alias Sportpal.Accounts.User
+    alias SportpalWeb.UserAuth
+    alias Sportpal.Repo
     plug Ueberauth
 
-    def callback(conn, params) do
-        IO.puts "+++"
-        IO.inspect(conn.assigns)
+    def callback(conn = %{assigns: %{ueberauth_auth: auth}}, params) do
+        users_params = %{email: auth.info.email}
+        changeset = User.email_changeset(%User{}, users_params)
+
+        insert_or_update_user(conn, changeset)
     end
 
     def request(conn, params) do
 
+    end
+
+    defp insert_or_update_user(conn, changeset) do
+        case Repo.get_by(User, email: changeset.changes.email) do
+            nil ->
+                case Repo.insert(changeset) do 
+                {:ok, user} ->
+                    conn
+                    |> put_flash(:info, "User created successfully.")
+                    |> UserAuth.log_in_user(user)
+                {:error, %Ecto.Changeset{} = changeset} ->
+                    conn
+                    |> put_flash(:info, "Something went wrong")
+                end
+            user ->
+                UserAuth.log_in_user(conn, user)
+        end
     end
 end
--- backend/priv/repo/migrations/20220210142146_create_users_auth_tables.exs
+++ backend/priv/repo/migrations/20220210142146_create_users_auth_tables.exs
@@ -6,7 +6,7 @@ defmodule Sportpal.Repo.Migrations.CreateUsersAuthTables do
 
     create table(:users) do
       add :email, :citext, null: false
-      add :hashed_password, :string, null: false
+      add :hashed_password, :string
       add :confirmed_at, :naive_datetime
       timestamps()
     end
