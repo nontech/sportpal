a2025b951b10d6c7fd9b83e78c0d99675a373b1a
aman03 <amanj@ntnu.no>
2022-10-02 21:11:27 +0200

    Redirect after onboarding fixed
    Changed redirection from /match page to / page after onboarding completed

--- backend/lib/sportpal_web/controllers/onboarding_controller.ex
+++ backend/lib/sportpal_web/controllers/onboarding_controller.ex
@@ -1,45 +1,43 @@
 defmodule SportpalWeb.OnboardingController do
-    use SportpalWeb, :controller
+  use SportpalWeb, :controller
 
-    alias Sportpal.Accounts
+  alias Sportpal.Accounts
 
+  def edit(conn, _params) do
+    changeset = Accounts.change_user_onboarding_data(conn.assigns.current_user)
+    render(conn, "edit.html", changeset: changeset)
+  end
 
-    def edit(conn, _params) do
-        changeset = Accounts.change_user_onboarding_data(conn.assigns.current_user)
+  def update(conn, %{"user" => user_params}) do
+    user = conn.assigns.current_user
+
+    case Accounts.update_user_onboarding_data(user, user_params) do
+      {:ok, _user} ->
+        conn
+        |> put_flash(:info, "Successfully Saved")
+        |> redirect(to: Routes.onboarding_path(conn, :edit_sports))
+
+      {:error, changeset} ->
         render(conn, "edit.html", changeset: changeset)
     end
+  end
 
-    def update(conn, %{"user" => user_params}) do
-        user = conn.assigns.current_user
-    
-        case Accounts.update_user_onboarding_data(user, user_params) do
-          {:ok, _user} ->
-            conn
-            |> put_flash(:info, "Successfully Saved")
-            |> redirect(to: Routes.onboarding_path(conn, :edit_sports))
-    
-          {:error, changeset} ->
-            render(conn, "edit.html", changeset: changeset)
-        end
-    end
+  def edit_sports(conn, _params) do
+    changeset = Accounts.change_user_sports(conn.assigns.current_user)
+    render(conn, "edit_sports.html", changeset: changeset)
+  end
 
-    def edit_sports(conn, _params) do
-        changeset = Accounts.change_user_sports(conn.assigns.current_user)
+  def update_sports(conn, %{"user" => user_params}) do
+    user = conn.assigns.current_user
+
+    case Accounts.update_user_sports(user, user_params) do
+      {:ok, _user} ->
+        conn
+        |> put_flash(:info, "Successfully Saved")
+        |> redirect(to: Routes.landing_page_path(conn, :index))
+
+      {:error, changeset} ->
         render(conn, "edit_sports.html", changeset: changeset)
     end
-
-    def update_sports(conn, %{"user" => user_params}) do
-        user = conn.assigns.current_user
-
-        case Accounts.update_user_sports(user, user_params) do
-            {:ok, _user} ->
-                conn
-                |> put_flash(:info, "Successfully Saved")
-                |> redirect(to: Routes.user_matches_path(conn, :index))
-
-            {:error, changeset} ->
-                render(conn, "edit_sports.html", changeset: changeset)
-        end
-    end
-
+  end
 end
