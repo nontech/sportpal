1a6111bdab636c6955bcfc21cd1859fe5c4da6a6
Mukesh Jaiswal <jais.mukesh@gmail.com>
2022-10-11 18:25:02 +0200

    Test env variable changed

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -9,8 +9,8 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :sportpal, Sportpal.Repo,
-  username: "amanjaiswal",
-  password: "",
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PWD"),
   database: "sportpal_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox,
