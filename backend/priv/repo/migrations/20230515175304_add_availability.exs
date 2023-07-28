ccbf9713bdb2d4003e3b7770316b140f9d7426df
Aman <aman.jaiswal@code.berlin>
2023-07-28 17:49:25 +0200

    Fixed failing test because of migration error

--- backend/priv/repo/migrations/20230515175304_add_availability.exs
+++
@@ -1,9 +0,0 @@
-defmodule Sportpal.Repo.Migrations.AddAvailability do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add :availability, {:array, :string}
-    end
-  end
-end
